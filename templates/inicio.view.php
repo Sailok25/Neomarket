8ce13313a658ab66defe4373a1732f9d4533c6b7
Sailok <albaayvi4@gmail.com>
2025-02-12 20:16:17 +0100

    css adaptado e inicio de ficha prod

---
+++ templates/inicio.view.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Urbanist:ital,wght@0,100..900;1,100..900&family=Viga&display=swap"
+        rel="stylesheet">
+    <title>Inicio | Neomarket</title>
+    <link rel="stylesheet" href="../../assets/css/base_pag.css">
+    <link rel="stylesheet" href="../../assets/css/global.css">
+    <link rel="stylesheet" href="../../assets/css/icons/iconos.css">
+</head>
+
+<body>
+    <header>
+        <div id="barra">
+            <div class="bordeado">
+                <a href="../../index.php"><span class="dashicons--arrow-left-alt2"></span></a>
+            </div>
+            <div class="logo">
+                <p>Neomarket.</p>
+            </div>
+            <div class="bordeado">
+                <span class="f7--cart"></span>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div id="all">
+            <h1>prova text</h1>
+        </div>
+    </main>
+    <footer>
+        <div id="iconos">
+            <div class="i_opcio"><a href="#"><span class="clarity--home-line"></span></a></div>
+            <div class="i_opcio"><a href="#"><span class="prime--bookmark"></span></a></div>
+            <div class="i_opcio"><a href="#"><span class="f7--cart"></span></a></div>
+            <div class="i_opcio"><a href="#"><span class="ion--notifications-outline"></span></a></div>           
+            <div class="i_opcio"><a href="#"><span class="mingcute--user-2-line"></span></a></div>
+        </div>
+    </footer>
+</body>
